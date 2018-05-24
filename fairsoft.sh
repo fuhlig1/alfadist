cc31142d0fa7a7b288bbf39315e859d00002eb62
Dennis Klein <d.klein@gsi.de>
2018-05-24 09:33:34 +0200

    Add proper modulefiles for the virtual packages
    which are generated from the package's dependency list

--- fairsoft.sh
+++ fairsoft.sh
@@ -4,43 +4,41 @@ requires:
   - generators
   - simulation
   - ROOT
-  - ZeroMQ
-  - nanomsg
   - boost
   - protobuf
   - flatbuffers
   - msgpack
   - DDS
-  - googletest
   - FairLogger
   - FairMQ
 ---
+#!/bin/bash -e
+
+# To add new dependencies it is sufficient to change the `requires:` field.
+# Modulefile will update deps automatically.
+
+MODULEFILE_REQUIRES=""
+for PKG in $REQUIRES; do
+  [[ $PKG != defaults* ]] || continue
+  PKG_UP=$(echo $PKG|tr '[:lower:]' '[:upper:]'|tr '-' '_')
+  MODULEFILE_REQUIRES="$MODULEFILE_REQUIRES $PKG/$(eval echo \$${PKG_UP}_VERSION-\$${PKG_UP}_REVISION)"
+done
+MODULEFILE_REQUIRES=$(echo $MODULEFILE_REQUIRES)
+
 # Modulefile
-mkdir -p etc/modulefiles
-cat > etc/modulefiles/$PKGNAME <<EoF
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+cat > "$MODULEFILE" <<EoF
 #%Module1.0
 proc ModulesHelp { } {
   global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+  puts stderr "Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 }
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+module-whatis "Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0                                                                            \\
-            ${GEANT3_VERSION:+GEANT3/$GEANT3_VERSION-$GEANT3_REVISION}                          \\
-            ${GEANT4_VMC_VERSION:+GEANT4_VMC/$GEANT4_VMC_VERSION-$GEANT4_VMC_REVISION}          \\
-            ${PROTOBUF_VERSION:+protobuf/$PROTOBUF_VERSION-$PROTOBUF_REVISION}                  \\
-            ${PYTHIA6_VERSION:+pythia6/$PYTHIA6_VERSION-$PYTHIA6_REVISION}                      \\
-            ${PYTHIA_VERSION:+pythia/$PYTHIA_VERSION-$PYTHIA_REVISION}                          \\
-            ${VGM_VERSION:+vgm/$VGM_VERSION-$VGM_REVISION}                                      \\
-            ${BOOST_VERSION:+boost/$BOOST_VERSION-$BOOST_REVISION}                              \\
-            ROOT/$ROOT_VERSION-$ROOT_REVISION                                                   \\
-            ${ZEROMQ_VERSION:+ZeroMQ/$ZEROMQ_VERSION-$ZEROMQ_REVISION}                          \\
-            ${NANOMSG_VERSION:+nanomsg/$NANOMSG_VERSION-$NANOMSG_REVISION}                      \\
-            ${DDS_ROOT:+DDS/$DDS_VERSION-$DDS_REVISION}                                         \\
-            ${FLATBUFFERS_VERSION:+flatbuffers/$FLATBUFFERS_VERSION-$FLATBUFFERS_REVISION}      \\
-            ${MSGPACK_VERSION:+msgpack/$MSGPACK_VERSION-$MSGPACK_REVISION}                      \\
-            ${GOOGLETEST_VERSION:+googletest/$GOOGLETEST_VERSION-$GOOGLETEST_REVISION}
+module load BASE/1.0 $MODULEFILE_REQUIRES
+# Our environment
+setenv FAIRSOFT_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 EoF
-MODULEDIR="$INSTALLROOT/etc/modulefiles"
-mkdir -p $MODULEDIR && rsync -a --delete etc/modulefiles/ $MODULEDIR
